632b9c9c90f30f5f5e371700bb58e007821facda
LianMary <liana.maria230604@gmail.com>
2025-11-16 14:28:29 -0300

    Arquivo inicial criado com SpringBoot

---
+++ src/main/java/com/liana/SystemBiblioteca/SystemBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.liana.SystemBiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liana/SystemBiblioteca/SystemBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liana.SystemBiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
